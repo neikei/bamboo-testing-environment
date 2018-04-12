89e59b0690a6eaf82390a5a0c285ede41af96e68
neikei <niklas@vosskoetter.org>
2018-04-12 18:22:41 +0200

    Updated Ansible to version 2.5

--- ansible/tools/install_ansible_in_Vagrantbox.sh
+++ ansible/tools/install_ansible_in_Vagrantbox.sh
@@ -2,14 +2,23 @@
 debian_version=`cat /etc/debian_version`
 ansible_installed=`which ansible | wc -l`
 
-if [ $ansible_installed -eq 0 ] && [[ $debian_version =~ ^8.*$ ]]; then
-  echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
+if [ $ansible_installed -eq 0 ]; then
+
+  # Install dirmngr if Debian major version is 9
+  if [[ $debian_version =~ ^9.*$ ]]; then
+    sudo apt install -y dirmngr
+  fi
+
+  # Add ansible 2.5 repository and install ansible
   sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-  sudo apt-get update -y
-  sudo apt-get install -y ansible
-elif [ $ansible_installed -eq 0 ] && [[ $debian_version =~ ^9.*$ ]]; then
-  sudo apt-get update -y
-  sudo apt-get install -y ansible
+  echo "deb http://ppa.launchpad.net/ansible/ansible-2.5/ubuntu xenial main" >> /etc/apt/sources.list.d/ansible.list
+  sudo apt update -y
+  sudo apt install -y ansible
+
 else
   echo "Ansible is already installed."
 fi
+
+echo "==> Ansible version <=="
+ansible --version
+echo "==> Ansible version <=="
